cfe742f317f2c11f1f7350b69c5f59db461d9f4a
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-03-21 14:51:45 +0500

    create a view for leadsList, leadsDetail

--- leads/models.py
+++ leads/models.py
@@ -8,10 +8,8 @@ class User(AbstractUser):
 class Lead(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    age = models.IntegerField(default=0)
+    age = models.PositiveIntegerField(default=0)
     agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
